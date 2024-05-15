8bff9db791f7e9a801e425595fcc9bdd11d4796b
zeuss1102 <emiliopuig3@gmail.com>
2024-05-14 22:51:45 -0600

    folder gifgrid

---
+++ proyecto-gif-emilio-creando/src/components/GifGrid.jsx
@@ -0,0 +1,32 @@
+// Importamos el componente GifItem 
+import{ GifItem}from'./GifItem';
+// Importamos el hook personalizado useFetchGifs
+import{ useFetchGifs}from'../hooks/useFetchGifs';
+
+// Definimos un componente funcional llamado GifGrid que toma una categoría como prop
+exportconstGifGrid=({category})=> {
+    // Usamos el hook useFetchGifs para obtener las imágenes y el estado de carga
+    const{images,isLoading}=useFetchGifs( category);
+    // Devolvemos el JSX que se renderizará
+    return (
+     <>
+     {/* // Mostramos la categoría */}
+     <h3>{category}</h3>
+     {/* // Si está cargando, mostramos un mensaje de carga */}
+     {
+     isLoading&&(<h2>Cargando...</h2>)
+     }
+     {/* // Creamos un div con la clase card-grid */ } 
+     <div className="card-grid">
+        {/* // Mapeamos las imágenes a componentes GifItem, cada uno con una clave única y las propiedades de la imagen */}
+        {images.map((image)=> ( 
+        <GifItem 
+        key={image.id}
+        {...image}
+        />
+    ))
+    }
+    </div>
+    </>
+    )
+}
